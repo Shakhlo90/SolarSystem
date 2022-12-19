9da794d808afb1f5c2c6016b00ce7382b7ab6526
Shakhlo <110711642+Shakhlo90@users.noreply.github.com>
2022-12-19 09:12:23 -0500

    it has issue

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Test for github");
+        System.out.println("this is issue for shakhlo branch");
     }
 }
